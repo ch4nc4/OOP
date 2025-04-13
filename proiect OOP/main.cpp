72bc23c178d705cd85a4d999699e3784f9abc9be
ch4nc4 <raluca-melinda.ionete@s.unibuc.ro>
2025-04-13 16:50:40 +0300

    overloaded >>op for Element, added initGame function

--- proiect OOP/main.cpp
+++ proiect OOP/main.cpp
@@ -12,7 +12,8 @@
 - functie de initializare a jocului (procesez datele din fisierul de configurare) -> functie statica
 - static!! bool ca sa verif daca AlchemyTable a fost init. -> if not => eroare
 - supraincarcare operator citire elemente
-- fisier config elemente
+- fisier config elemente *
+- functie in care decid daca elementul este endElement sau nu
 
 */
 
@@ -25,10 +26,47 @@
 using namespace std;
 
 ifstream recipeFile("recipes.txt");
+ifstream elemFile("elements.txt");
+
+// --- functii non-membre ---
+
+std::istream& operator>>(std::istream& in, AlchemyTable::Element& other){
+    in >> other.name;
+    return in;
+}
+
+void findRecipe(const string givenString, string &res, string &elem1, string &elem2){
+    // aici parsam o reteta si "returnam" numele celor 3 elemente 
+    int i = 0;
+    while(givenString[i] != ':'){
+        res = res + givenString[i];
+        i++;
+    }
+    i++;
+
+    while(givenString[i] != '+'){
+        elem1 = elem1 + givenString[i];
+        i++;
+    }
+    i++;
+
+    while(givenString[i] != '\0'){
+        elem2 = elem2 + givenString[i];
+        i++;
+    }
+}
+
+// --- sfarsit functii non-membre ---
+
+
+// --- functii membre Element ---
 
 AlchemyTable::Element::Element(string name) {
     endElement = true;
     this->name = name;
+}
+
+AlchemyTable::Element::Element() {
 }
 
 bool AlchemyTable::Element::operator==(Element &other) {
@@ -59,16 +97,42 @@ AlchemyTable::Element AlchemyTable::Element::operator+(const Element &other)cons
     else{
         //throw error;
     }
-   
 }
 
-AlchemyTable::AlchemyTable(vector<Element> elements, map<pair<Element, Element>, Element> recipes){
+// --- sfarsit functii membre Element ---
+
+
+// ---functii membre AlchemyTable ---
+
+AlchemyTable::AlchemyTable(vector<Element> elements, map<pair<Element, Element>, Element> recipes, bool initTable = false){
 
 }
 
 void AlchemyTable::initGame(const char* fileName){
-    
+    AlchemyTable::Element elemName;
+    while(elemFile >> elemName){
+        elements.insert({elemName.getName(), elemName});
+    }
+
+    string readRecipe;
+    while(recipeFile >> readRecipe){
+        string res, elem1, elem2;
+        res = elem1 = elem2 = "";
+        findRecipe(readRecipe, res, elem1, elem2);
+
+        AlchemyTable::Element result, el1, el2;
+        result.setName(res);
+        el1.setName(elem1);
+        el2.setName(elem2);
+
+        recipes.insert({{el1, el2}, res});
+    }
+
+    initTable = true;
 }
+
+// --- sfarsit functii membre AlchemyTable ---
+
 
 int main()
 {
--- proiect OOP/main.h
+++ proiect OOP/main.h
@@ -5,30 +5,39 @@
 #include<vector>
 using namespace std;
 
-class AlchemyTable{
-    class Element{
-        string name;
-        bool endElement;
-        public:
-            Element(string name);
-    
-            bool operator==(Element &other);
-    
-            string getName() const;
-    
-            void setName(string newName);
-    
-            bool getType() const;
-    
-            void setType(bool newType);
+void findRecipe(const string givenString, string &res, string &elem1, string &elem2);
 
-            Element operator+(const Element &other)const;
-    
-    };
-    static map<string,Element> elements;
-    static map<pair<Element, Element>, Element> recipes;
+class AlchemyTable{
     public:
-        AlchemyTable(vector<Element> elements, map<pair<Element, Element>, Element> recipes);
+        class Element{
+            string name;
+            bool endElement;
+            public:
+                Element(string name);
+
+                Element();
+        
+                bool operator==(Element &other);
+        
+                string getName() const;
+        
+                void setName(string newName);
+        
+                bool getType() const;
+        
+                void setType(bool newType);
+
+                Element operator+(const Element &other)const;
+
+                friend std::istream& operator>>(std::istream& in, Element &other);
+        
+        };
+    private:
+        static map<string,Element> elements;
+        static map<pair<Element, Element>, Element> recipes;
+        static bool initTable;
+    public:
+        AlchemyTable(vector<Element> elements, map<pair<Element, Element>, Element> recipes, bool initTable = false);
 
         static void initGame(const char* fileName);
     
