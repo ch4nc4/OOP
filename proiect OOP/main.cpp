df6ea590b0515a7a151c0e39443c223c99d23319
ch4nc4 <raluca-melinda.ionete@s.unibuc.ro>
2025-05-04 18:50:08 +0300

    added SFML packages

--- proiect OOP/main.cpp
+++ proiect OOP/main.cpp
@@ -33,6 +33,7 @@ design patern
 #include<vector>
 #include<fstream>
 #include<map>
+#include <SFML/Window.hpp>
 using namespace std;
 
 ifstream recipeFile("recipes.txt");
@@ -179,5 +180,18 @@ bool AlchemyTable::verifInitGame(){
 
 int main()
 {
+    sf::Window window(sf::VideoMode({800, 600}), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        while (const std::optional event = window.pollEvent())
+        {
+            // "close requested" event: we close the window
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+    }
    return 0;
 }
